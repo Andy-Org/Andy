d56f17a74e6e9034ac38d6f3cc21d20bdaa600f8
Fred Nikgohar <frednikgohar@gmail.com>
2015-10-01 14:53:43 -0700

    Added dFlag for Verbose debug mode
    I'm using dFlag (eventually globally) for verbose debug messages on the Serial Monitor.
    
    The code is like this:
    
    if (dFlag == 1){
       Serial.println("Some Debug Message") }

--- IR_RxTx/send.ino
+++ IR_RxTx/send.ino
@@ -1,5 +1,7 @@
 #include "IRSendRev.h"
 
+#define dFlag 0   // Set this flag to 1 if you want debug on the Serial Monitor
+
 #define BIT_LEN         0
 #define BIT_START_H     1
 #define BIT_START_L     2
@@ -14,19 +16,19 @@ unsigned char dtaSend[20];
 
 void dtaInit()
 {
-    dtaSend[BIT_LEN]        = 11;			// all data that needs to be sent
-    dtaSend[BIT_START_H]    = 179;			// the logic high duration of "Start"
-    dtaSend[BIT_START_L]    = 90;			// the logic low duration of "Start"
-    dtaSend[BIT_DATA_H]     = 11;			// the logic "long" duration in the communication
-    dtaSend[BIT_DATA_L]     = 33;			// the logic "short" duration in the communication
+    dtaSend[BIT_LEN]        = 11;      // all data that needs to be sent
+    dtaSend[BIT_START_H]    = 179;      // the logic high duration of "Start"
+    dtaSend[BIT_START_L]    = 90;     // the logic low duration of "Start"
+    dtaSend[BIT_DATA_H]     = 11;     // the logic "long" duration in the communication
+    dtaSend[BIT_DATA_L]     = 33;     // the logic "short" duration in the communication
     
-    dtaSend[BIT_DATA_LEN]   = 6;			// Number of data which will sent. If the number is other, you should increase or reduce dtaSend[BIT_DATA+x].
+    dtaSend[BIT_DATA_LEN]   = 6;      // Number of data which will sent. If the number is other, you should increase or reduce dtaSend[BIT_DATA+x].
     
-    dtaSend[BIT_DATA+0]     = 128;			// data that will sent
+    dtaSend[BIT_DATA+0]     = 128;      // data that will sent
     dtaSend[BIT_DATA+1]     = 127;
     dtaSend[BIT_DATA+2]     = 192;
     dtaSend[BIT_DATA+3]     = 63;
-	dtaSend[BIT_DATA+4]     = 192;
+  dtaSend[BIT_DATA+4]     = 192;
     dtaSend[BIT_DATA+5]     = 63;
 }
 
@@ -38,9 +40,12 @@ void setup()
 
 void loop()
 {
+    if (dFlag == 1){
     Serial.print("Ready to send data now...  ");
+    }
     IR.Send(dtaSend, 38);
+    if (dFlag == 1){
     Serial.println("Data Sent Bitches!");
-    
+    }
     delay(2000);
 }
